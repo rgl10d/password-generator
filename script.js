ba7f7250d25c464e298c09f9f4b0fc27810941d8
Garrett Lee <rgl10d@gmail.com>
2020-09-22 13:51:28 -0400

    added all confirms/prompts and created the character object

--- script.js
+++ script.js
@@ -7,10 +7,25 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 function generatePassword(){
+
+  var passwordLength = prompt("How many characters would you like in your password?");
+  var passwordLowerCase = confirm("Click OK if you want to include lowercase characters.");
+  var passwordUpperCase = confirm("Click OK if you want to include uppercase characters.");
+  var passwordNumber = confirm("Click OK if you want to include numeric characters.");
+  var passwordSpecial = confirm("Click OK if you want to include special characters.");
+
+  var characters = {
+    alphabet: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+    numbers: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+    special: ["!", "#", "$", "%", "&", "(", ")", "*", "+" , "-" , "/", "<", "=", ">", "?", "@", "^"],
+  }
+
+  if (passwordLength >= 8 && passwordLength <= 128){
+    console.log(passwordLength);
+  }
 
   return "This will be replaced by my generated password.";
 }
